2f2d1829c983c0a86a4b448a0d8f9f0f363b8ec5
Junior Dering <juniordering@hotmail.com>
2023-11-04 11:09:53 -0400

    feat: improving style and building input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
-import { styles } from "./styles";
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { styles } from './styles'
 
-export default function Home () {
-  return(
+export const Home = () => {
+  const handleParticipantAdd = () => {
+    console.log('Você adicionou um participante')
+  }
+  return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do Evento
-      </Text>
-      <Text style={styles.eventDate}>
-        Sabado, 4 de Novembro de 2023
-      </Text>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sabado, 4 de Novembro de 2023</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
     padding: 24,
   },
   eventName: {
-    color: '#fdfcfe',
+    color: '#fff',
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 48,
@@ -15,5 +15,34 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: '#6b6b6b',
     fontSize: 16,
+  },
+
+  input: {
+    height: 56,
+    backgroundColor: '#1f1e25',
+    borderRadius: 5,
+    color: '#fff',
+    padding: 16,
+    fontSize: 16,
+    flex: 1,
+    marginRight: 12,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 24,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31cf67',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginRight: 42,
   },
 })
